dce94f7a3af89f9650f70f570d13b1df4c1082fa
Brion <info@brionmario.com>
2023-05-07 10:39:06 +0530

    Fix button issue

--- src/apps/kfone-customer-support/src/components/buttons/primary-button.tsx
+++ src/apps/kfone-customer-support/src/components/buttons/primary-button.tsx
@@ -38,8 +38,9 @@ export const PrimaryButton: FC<PrimaryButtonProps> = ({
   const classes: string = clsx(
     'flex text-white right-2.5 bottom-2.5 bg-primary',
     'focus:ring-4 focus:outline-none focus:ring-blue-300',
-    'font-medium rounded-lg text-sm px-4 py-2 opacity-100',
+    'font-medium rounded-lg text-sm px-4 py-2',
     {
+      'opacity-100': !disabled,
       'opacity-50': disabled,
     },
     className,
